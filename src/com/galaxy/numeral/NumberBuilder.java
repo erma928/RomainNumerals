e0dd12a4d8946fecb1d12b37063aa2e3a402425a
Feng Jimin <joe5338.cn@gmail.com>
2019-05-14 20:01:01 +0800

    doc for builder

--- src/com/galaxy/numeral/NumberBuilder.java
+++ src/com/galaxy/numeral/NumberBuilder.java
@@ -3,6 +3,10 @@ package com.galaxy.numeral;
 import com.galaxy.exceptions.NumberFormatException;
 import com.galaxy.numeral.parser.NumberParser;
 
+/**
+ * builder for numerals; all creations of numerals are through here
+ * @param <T>
+ */
 public abstract class NumberBuilder<T extends Number> {
 
     protected NumberParser numberParser;
